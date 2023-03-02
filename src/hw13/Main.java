df9df74dfc80d79e6536855850fbee9e8c2dd1c3
rom064 <rom064@mail.ru>
2023-03-02 22:26:09 +0500

    Выполнил ДЗ 13 Методы объектов

--- src/hw13/Main.java
+++ src/hw13/Main.java
@@ -1,7 +1,19 @@
 package hw13;
 
 public class Main {
+    /* Реализуйте методы toString, equals и hashCode в классах Author и Book, которые вы создали на прошлом уроке.
+Обратите внимание, что toString книги не должен дублировать код из toString автора,
+ а должен делегировать (вызывать) его версию метода. */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        Book lolita = new Book("Набоков", "\"Лолита\"", 1955);
+        Author nabokov = new Author("Владимир Владимирович", "Набоков");
+        System.out.println(lolita);
+        System.out.println(nabokov);
+        System.out.println();
+        Book idiot = new Book("Достоевский", "\"Идиот\"", 1867);
+        Author dostoevsky = new Author("Федор Михайлович", "Достоевский");
+        System.out.println(idiot);
+        System.out.println(dostoevsky);
+        System.out.println(idiot.equals(lolita));
+        System.out.println(nabokov.equals(dostoevsky));
+    }}
